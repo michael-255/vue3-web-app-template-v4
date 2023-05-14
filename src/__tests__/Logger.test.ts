1531a7c5d4a36b7e57fcd6e641c245f18c3d05d0
Michael Joy <kodiakchaos@gmail.com>
2023-05-13 21:40:22 -0400

    logger test corrections

--- src/__tests__/Logger.test.ts
+++ src/__tests__/Logger.test.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect, beforeEach, vi } from 'vitest'
-import Logger, { ApplicationLogger } from '@/services/Logger'
+import Logger from '@/services/Logger'
+import { AppName } from '@/types/general'
 
 describe('Logger service', () => {
   const baseStyle = 'border-radius: 3px; padding: 2px 4px; color: white; background-color:'
@@ -9,47 +10,40 @@ describe('Logger service', () => {
   const warnColor = '#ff6f00'
   const errorColor = '#C10015'
   const message = 'Test logger message!'
-  let logger: ApplicationLogger
   let name: string
-  let instanceCounter = 0
 
   beforeEach(() => {
-    name = `TestLogger-${instanceCounter++}`
-    logger = new ApplicationLogger(name)
+    name = AppName
     vi.clearAllMocks()
-  })
-
-  test('Logger is an instance of ApplicationLogger', () => {
-    expect(Logger).toBeInstanceOf(ApplicationLogger)
   })
 
   test('print calls stylized console.log', () => {
     const logSpy = vi.spyOn(console, 'log')
-    logger.print(message)
+    Logger.print(message)
     expect(logSpy).toHaveBeenCalledWith(`%c${name}`, `${baseStyle} ${printColor};`, message)
   })
 
   test('debug calls stylized console.log', () => {
     const logSpy = vi.spyOn(console, 'log')
-    logger.debug(message)
+    Logger.debug(message)
     expect(logSpy).toHaveBeenCalledWith(`%c${name}`, `${baseStyle} ${debugColor};`, message)
   })
 
   test('info calls stylized console.log', () => {
     const logSpy = vi.spyOn(console, 'log')
-    logger.info(message)
+    Logger.info(message)
     expect(logSpy).toHaveBeenCalledWith(`%c${name}`, `${baseStyle} ${infoColor};`, message)
   })
 
   test('warn calls stylized console.warn', () => {
     const logSpy = vi.spyOn(console, 'warn')
-    logger.warn(message)
+    Logger.warn(message)
     expect(logSpy).toHaveBeenCalledWith(`%c${name}`, `${baseStyle} ${warnColor};`, message)
   })
 
   test('error calls stylized console.error', () => {
     const logSpy = vi.spyOn(console, 'error')
-    logger.error(message)
+    Logger.error(message)
     expect(logSpy).toHaveBeenCalledWith(`%c${name}`, `${baseStyle} ${errorColor};`, message)
   })
 })
